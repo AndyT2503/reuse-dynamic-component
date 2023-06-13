78b7eeaa412fe1108f91b0c4a0b4f6f87afdc3ae
tuhoang <htu25399@gmail.com>
2023-06-13 23:20:40 +0700

    demo reuse view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ComponentRef,
+  Type,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'reuse-dynamic-component';
+  @ViewChild('vc', { read: ViewContainerRef }) vc!: ViewContainerRef;
+  cacheComponent = new Map<
+    'first' | 'second',
+    ComponentRef<FirstComponent | SecondComponent>
+  >();
+
+  onCheck(event: Event): void {
+    if (event.target instanceof HTMLInputElement) {
+      const isChecked = event.target.checked;
+      const id = event.target.id as 'first' | 'second';
+      this.#onSelectComponent(id, isChecked);
+    }
+  }
+
+  #onSelectComponent(type: 'first' | 'second', isChecked: boolean): void {
+    const dynamicComponentOption: Record<
+      'first' | 'second',
+      Type<FirstComponent | SecondComponent>
+    > = {
+      first: FirstComponent,
+      second: SecondComponent,
+    };
+    const component = dynamicComponentOption[type];
+    const viewRef = this.cacheComponent.get(type);
+    if (!isChecked) {
+      this.vc.detach(this.vc.indexOf(viewRef!.hostView));
+      return;
+    }
+    if (!viewRef) {
+      const componentRef = this.vc.createComponent(component);
+      this.cacheComponent.set(type, componentRef);
+      this.vc.insert(componentRef.hostView);
+      return;
+    }
+    this.vc.insert(viewRef.hostView);
+  }
 }
